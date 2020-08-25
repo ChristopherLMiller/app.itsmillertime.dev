ece3ff54364a0ea8a46ba28392034ed0f2ef4616
ChristopherLMiller <chris@christopherleemiller.me>
2020-08-25 12:17:40 -0400

    fix header to be responsive

--- src/components/layout/elements/Header.tsx
+++ src/components/layout/elements/Header.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { FunctionComponent } from "react";
 
 const StyledHeader = styled.div`
-  min-height: 350px;
+  padding-bottom: 100px;
   position: relative;
 `;
 
@@ -10,19 +10,30 @@ const HeaderBackground = styled.div`
   position: absolute;
   left: 50%;
   top: 50%;
-  width: 110%;
-  height: 100%;
+  width: 150%;
+  min-height: 350px;
   z-index: -1;
   background: var(--color-red);
-  transform: rotateZ(-5deg) translate(-50%, -150%);
+  transform: rotateZ(-5deg) translate(-50%, -100%);
   opacity: 0.8;
+  transition: all 0.5s;
+
+  @media (min-width: 800px) {
+    transform: rotateZ(-5deg) translate(-50%, -110%);
+  }
+  @media (min-width: 1000px) {
+    transform: rotateZ(-5deg) translate(-50%, -120%);
+  }
+  @media (min-width: 1400px) {
+    transform: rotateZ(-5deg) translate(-50%, -130%);
+  }
 `;
 
 const HeaderDescription = styled.h2`
   font-family: var(--font-main);
   font-weight: 100;
-  font-size: var(--h2-size);
-  padding-left: 2rem;
+  font-size: var(--h2-responsive);
+  padding: 0rem 2rem;
   color: var(--color-white);
   margin-bottom: 0;
 `;
@@ -31,7 +42,7 @@ const HeaderTitle = styled.h1`
   font-family: var(--font-main);
   font-weight: 300;
   font-size: var(--h1-responsive);
-  padding-left: 3.5rem;
+  padding-left: 6rem;
   color: var(--color-white);
   margin-top: 0;
 `;
