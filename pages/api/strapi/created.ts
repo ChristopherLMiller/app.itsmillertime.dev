f871030579861b634c2e2d5d150a16acd2fe11cc
ChristopherLMiller <chris@christopherleemiller.me>
2020-08-26 16:34:16 -0400

    only log the body

--- pages/api/strapi/created.ts
+++ pages/api/strapi/created.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req);
+  console.log(req.body);
   res.send("Test");
   res.end();
 };
