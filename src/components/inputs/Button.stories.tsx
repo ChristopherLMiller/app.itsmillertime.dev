3383d8c1699480a5204d5107b9ddb864911b4031
ChristopherLMiller <chris@christopherleemiller.me>
2020-09-16 04:37:35 -0400

    work on stories

--- src/components/inputs/Button.stories.tsx
+++ src/components/inputs/Button.stories.tsx
@@ -1,3 +1,4 @@
+import { GlobalStyles } from "../../../styles/default";
 import Button from "./Button";
 
 export default {
@@ -5,13 +6,33 @@ export default {
   component: Button,
 };
 
-const Template = (args: typeof Button) => ({
-  component: Button,
-  props: args,
-});
+const Template = (args) => (
+  <>
+    <Button {...args} />
+    <GlobalStyles />
+  </>
+);
+
+export const Normal = Template.bind({});
+
+Normal.args = {
+  children: "Button",
+  isDisabled: false,
+  isSubmitting: false,
+};
 
 export const Disabled = Template.bind({});
+
 Disabled.args = {
   isDisabled: true,
+  isSubmitting: false,
   children: "Disabled",
+};
+
+export const Submitting = Template.bind({});
+
+Submitting.args = {
+  isSubmitting: true,
+  isDisabled: false,
+  children: "Submitting",
 };
