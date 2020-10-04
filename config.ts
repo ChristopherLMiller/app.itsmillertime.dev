400b63d68f6b029aa0d5b1c654fcddf36a242870
ChristopherLMiller <chris@christopherleemiller.me>
2020-10-04 09:30:58 -0400

    move endpoint for CMS to .env

--- config.ts
+++ config.ts
@@ -1,7 +1,5 @@
 // Client side things only, don't put things here that shouldn't be public
-export const API_ENDPOINT = `https://strapi.christopherleemiller.me`;
-export const GRAPHQL_ENDPOINT = `${API_ENDPOINT}/graphql`;
-export const STRAPI_ENDPOINT = `https://strapi.christopherleemiller.me`;
+export const GRAPHQL_ENDPOINT = `${process.env.STRAPI_URL}/graphql`;
 export const CLOUDINARY_URL = `https://res.cloudinary.com`;
 export const CLOUDINARY_CLOUD = `christopherleemiller`;
 export const CLOUDINARY_FOLDER = `clm-new`;
