941329663bc7f30ed8c1175c00bc97c5e8526a18
ChristopherLMiller <chris@christopherleemiller.me>
2020-10-08 13:21:42 -0400

    delete page taht was never meant to exist

--- pages/test2.tsx
+++
@@ -1,9 +0,0 @@
-import PageLayout from "src/layout/PageLayout";
-
-const test2Page = () => (
-  <PageLayout title="Test 2" description="Test">
-    <div>Test</div>
-  </PageLayout>
-);
-
-export default test2Page;
