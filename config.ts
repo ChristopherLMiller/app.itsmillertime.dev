6166eadd2876a2595a205a456ca4d092f28b8186
ChristopherLMiller <chris@christopherleemiller.me>
2020-11-27 05:10:39 -0500

    fix url for strapi

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // Client side things only, don't put things here that shouldn't be public
-export const GRAPHQL_ENDPOINT = `${process.env.STRAPI_URL}/graphql`;
+export const GRAPHQL_ENDPOINT = `${process.env.NEXT_PUBLIC_STRAPI_URL}/graphql`;
 export const CLOUDINARY_URL = `https://res.cloudinary.com`;
 export const CLOUDINARY_CLOUD = `christopherleemiller`;
 export const CLOUDINARY_FOLDER = `clm-new`;
