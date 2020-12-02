f0d7599fe4dd29b3d5dff4a6a7cdbd60318bf693
ChristopherLMiller <chris@christopherleemiller.me>
2020-12-02 11:06:50 -0500

    Add ability to make grid masonry like

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,19 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 interface iGrid {
   columns: number;
   background?: string;
   gap?: string;
   min?: string;
+  masonry?: boolean;
 }
 
 export const Grid = styled.div<iGrid>`
   display: grid;
   grid-template-columns: repeat(
     auto-fit,
-    minmax(${(props) => props.min || "350px"}, 1fr)
+    minmax(${(props) => props.min || '350px'}, 1fr)
   );
-  grid-gap: ${(props) => props.gap || "0px"};
+  grid-template-rows: ${(props) => (props.masonry ? 'masonry' : 'auto')};
+  grid-gap: ${(props) => props.gap || '0px'};
   background: ${(props) => props.background};
   margin-bottom: 50px;
 `;
