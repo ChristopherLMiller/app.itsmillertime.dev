0b1a68029db180270d23b7a2e2a2c6030f370bad
ChristopherLMiller <chris@christopherleemiller.me>
2020-12-16 07:41:37 -0500

    update scroll color for contrast

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -8,7 +8,7 @@ const ScrollTopDiv = styled(motion.div)`
   position: fixed;
   right: 30px;
   bottom: 30px;
-  color: var(--color-red-dark);
+  color: var(--color-white-100);
   justify-content: center;
   align-content: center;
   font-size: 6rem;
