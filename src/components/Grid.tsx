c1b0fc1205d176ec076187e1ba3651de0e3af6b5
ChristopherLMiller <chris@christopherleemiller.me>
2020-12-16 09:18:40 -0500

    fix grid under size

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -9,13 +9,13 @@ interface iGrid {
 
 export const Grid = styled.div<iGrid>`
   display: grid;
-  grid-template-columns: repeat(auto-fit, , 1fr);
+  grid-template-columns: 1fr;
   grid-template-rows: ${(props) => (props.masonry ? 'masonry' : 'auto')};
   grid-gap: ${(props) => props.gap || '0px'};
   background: ${(props) => props.background};
   margin-bottom: 50px;
 
-  @media (min-width: ${(props) => props.min || '300px'}) {
+  @media (min-width: ${(props) => props.min || '300'}) {
     grid-template-columns: repeat(
       auto-fit,
       minmax(${(props) => props.min || '300px'}, 1fr)
