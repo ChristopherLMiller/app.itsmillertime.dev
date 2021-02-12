225ed97b93ced547ad4c0307a7454ea452a867b1
ChristopherLMiller <chris@christopherleemiller.me>
2021-02-12 09:34:40 -0500

    fix: :mag: Add articles and galleries to the sitemap
    Add entires for articles and galleries to the sitemap

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -1,12 +1,31 @@
 import { GetServerSideProps } from 'next';
 import { getServerSideSitemap } from 'next-sitemap';
+import fetch from 'src/utils/functions/fetch';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // Fetch all dynamic content here
+  const articles = await fetch
+    .get(`/articles?_publicationState=live&_sort=published_at:DESC`)
+    .then(({ data }) =>
+      data?.map((article) => ({
+        loc: `${process.env.NEXT_PUBLIC_SITE_URL}/article/${article?.slug}`,
+        lastmod: article?.updatedAt,
+      }))
+    );
 
-  const fields = [];
+  const galleries = await fetch
+    .get(`/galleries?_publicationState=live&_sort=published_at:DESC`)
+    .then(({ data }) =>
+      data?.map((gallery) => ({
+        loc: `${process.env.NEXT_PUBLIC_SITE_URL}/gallery/${gallery?.slug}`,
+        lastmod: gallery?.updatedAt,
+      }))
+    );
 
-  return getServerSideSitemap(ctx, fields);
+  const fields = [articles, galleries];
+  return getServerSideSitemap(ctx, fields.flat(1));
 };
 
-export default () => {};
+const SiteMapPage = () => {};
+
+export default SiteMapPage;
