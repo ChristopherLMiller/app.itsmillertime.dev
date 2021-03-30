c90d92bcbd7e09b00bad7a47578253ecb5cd2a98
ChristopherLMiller <chris@christopherleemiller.me>
2021-03-30 08:09:47 -0400

    update next

--- next.config.js
+++ next.config.js
@@ -53,6 +53,9 @@ const nextConfig = {
   serverRuntimeConfig: {
     rootDir: __dirname,
   },
+  future: {
+    webpack5: true,
+  },
   webpack: (config, options) => {
     // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
     // @sentry/node will run in a Node.js environment. @sentry/node will use
