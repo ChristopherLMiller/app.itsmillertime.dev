f8d07974153b00df078add32dace08b589cd70f6
ChristopherLMiller <chris@christopherleemiller.me>
2022-04-22 06:12:44 -0400

    fix: :art: change clockify functions to use the backend api now

--- src/utils/getBuildTime.ts
+++ src/utils/getBuildTime.ts
@@ -1,12 +1,20 @@
+import { ApiEndpoint } from "config";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getBuildTime = async (id: string): Promise<any> => {
-  const response = await fetch(`/api/models/buildtime`, {
-    method: `POST`,
-    body: JSON.stringify({
-      project_id: id,
-    }),
+  const response = await fetch(`${ApiEndpoint}/clockify/buildtime/${id}`, {
+    method: `GET`,
+    credentials: "omit",
+    headers: {
+      "x-api-key":
+        "46c446ba8878e0fed3da1901828f9a5539989b567915d82e894a63207c1708be",
+    },
   });
 
-  const data = await response.json();
-  return data?.duration;
+  const { data, statusCode, error } = await response.json();
+  if (statusCode === 200) {
+    return data?.duration;
+  } else {
+    throw new Error(error);
+  }
 };
